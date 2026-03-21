40cf18c841b9b7adb52b7ec654dca290a029c599
Dejan Milicic <dejan.milicic@jetbrains.com>
2026-03-21 22:12:49 +0100

    Highest pair task + test

---
+++ src/lab2/task1/FindPairOfHighestSum.kt
@@ -0,0 +1,33 @@
+package lab2.task1
+
+import lab2.common.isEqualsTo
+
+
+/**
+ * Task 1: Find a Pair that adds to the Highest Sum
+ *
+ * Write an extension function for a list of ints that returns a pair of numbers from the list
+ * that make the highest sum. If there is more than one pair that makes the highest sum,
+ * return the last pair.
+ *
+ * Constraints:
+ * List has at least two integers.
+ * Items in the list are in range -1000..1000
+ *
+ */
+
+internal fun List<Int>.findHighestSumPair(): Pair<Int, Int> {
+    TODO("Implement me!!")
+}
+
+fun main() {
+    val nums = listOf(743, 284, 677, -753, 995, -934, 102, 903, -83, -760, 77, -420)
+    val expectedPair = Pair(995, 903)
+    val actualPair = nums.findHighestSumPair()
+
+    println("Pair that has highest sum in list $nums is $actualPair.")
+
+    require(expectedPair.isEqualsTo(actualPair)) {
+        "Actual pair that has highest sum in list $nums was $actualPair, but expected was $expectedPair"
+    }
+}
