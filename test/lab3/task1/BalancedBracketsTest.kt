43a3516502dfdf55ed61564868bc7529e3c04150
Dejan Milicic <dejan.milicic@jetbrains.com>
2026-03-22 15:32:24 +0100

    Balanced brackets - task + tests

---
+++ test/lab3/task1/BalancedBracketsTest.kt
@@ -0,0 +1,41 @@
+package lab3.task1
+
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+class BalancedBracketsTest {
+
+    @Test
+    fun `test - verify expression is balanced`() {
+        assertTrue(isExpressionBalanced("{}"))
+        assertTrue(isExpressionBalanced("()"))
+        assertTrue(isExpressionBalanced("[]"))
+        assertTrue(isExpressionBalanced("{[()]}"))
+        assertTrue(isExpressionBalanced("{[()]()}"))
+        assertTrue(isExpressionBalanced("[{}]"))
+        assertTrue(isExpressionBalanced("((()))"))
+        assertTrue(isExpressionBalanced("{[()]()[]}"))
+        assertTrue(isExpressionBalanced("{{}}"))
+        assertTrue(isExpressionBalanced("[[()]]"))
+        assertTrue(isExpressionBalanced("{{([][])}()}"))
+        assertTrue(isExpressionBalanced("((()))"))
+        assertTrue(isExpressionBalanced("(){}[]"))
+    }
+
+    @Test
+    fun `test - verify expression is unbalanced`() {
+        assertFalse(isExpressionBalanced("{"))
+        assertFalse(isExpressionBalanced("}"))
+        assertFalse(isExpressionBalanced("[["))
+        assertFalse(isExpressionBalanced("))"))
+        assertFalse(isExpressionBalanced(")("))
+        assertFalse(isExpressionBalanced("{[()]())"))
+        assertFalse(isExpressionBalanced("{[(])}"))
+        assertFalse(isExpressionBalanced("{[}]"))
+        assertFalse(isExpressionBalanced("{[}"))
+        assertFalse(isExpressionBalanced("}{"))
+        assertFalse(isExpressionBalanced("{{[}}"))
+        assertFalse(isExpressionBalanced("()("))
+        assertFalse(isExpressionBalanced("[(])"))
+    }
+}
