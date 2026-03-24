4028c6449b0cfbd798633f79a385055e49453fc8
Dusan <dusan@example.com>
2026-03-24 23:48:28 +0100

    Resenje piramida

--- src/lab1/task2/PrintPyramid.kt
+++ src/lab1/task2/PrintPyramid.kt
@@ -56,8 +56,36 @@ package lab1.task2
  *
  */
 
+internal fun printPyramidOld(level: Int) {
+    require(level in 1..15)
+
+    for (i in 1..level) {
+
+        for (s in 1..level - i) {
+            print(" ")
+        }
+
+        for (j in 1..i*2-1) {
+            print("*")
+        }
+
+        for (s in 1..level - i) {
+            print(" ")
+        }
+
+        println()
+    }
+
+}
+
 internal fun printPyramid(level: Int) {
-    TODO("Implement me!!!")
+    require(level in 1..15)
+
+    for (i in 1..level) {
+        val spaces = " ".repeat(level - i)
+        val asteriks = "*".repeat(i*2-1)
+        println(spaces + asteriks + spaces)
+    }
 }
 
 fun main() {
