b819849dd5c8604a660e7229be802a13bd87ee1b
Dusan <dusan@example.com>
2026-03-24 23:21:27 +0100

    Resenje Zadatka 3

--- src/lab1/task3/ScrabbleWordScoreCalculator.kt
+++ src/lab1/task3/ScrabbleWordScoreCalculator.kt
@@ -32,8 +32,23 @@ package lab1.task3
  */
 
 internal fun calculateWordScrabbleScore(word: String): Int {
-    TODO("Implement me!!!")
+    var suma = 0
+    for (ch in word) {
+        suma += skrablMapa[ch.uppercaseChar()] ?: error("$ch nije validna skrabl rec")
+    }
+
+    return suma
 }
+
+private val skrablMapa = mapOf(
+    'A' to 1, 'E' to 1, 'I' to 1, 'O' to 1, 'U' to 1, 'L' to 1, 'N' to 1, 'R' to 1, 'S' to 1, 'T' to 1,
+    'D' to 2, 'G' to 2,
+    'B' to 3, 'C' to 3, 'M' to 3, 'P' to 3,
+    'F' to 4, 'H' to 4, 'V' to 4, 'W' to 4, 'Y' to 4,
+    'K' to 5,
+    'J' to 8, 'X' to 8,
+    'Q' to 10, 'Z' to 10
+)
 
 fun main() {
     val word = "faculty"
