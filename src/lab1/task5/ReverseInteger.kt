a3ccd4734a4b88f451af423b5d61aef49619ab7b
Markeyen <ljubojamateja@gmail.com>
2026-03-24 20:51:14 +0100

    Uradjen zadatak 5

--- src/lab1/task5/ReverseInteger.kt
+++ src/lab1/task5/ReverseInteger.kt
@@ -29,10 +29,26 @@ import kotlin.math.abs
  */
 
 internal fun reverseInteger(x: Int): Int {
-    TODO("Implement me!!!")
+    if (x !in -1_000_000..1_000_000)
+    {
+        throw ArithmeticException()
+    }
+
+    var i = x
+    var obrunut = 0
+    var tmp = x
+
+    while (tmp != 0 ) {
+        val broj = tmp % 10
+        obrunut = obrunut * 10 + broj
+        tmp /= 10
+
+    }
+
+    return obrunut
 }
 
 fun main() {
-    val integer = -321
+    val integer = 120
     println("Reverse integer of number $integer is ${reverseInteger(integer)}")
 }
