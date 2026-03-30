056654d9bab42d44f3927dddbb5ea5c24723d953
Dejan Milicic <dejan.milicic@jetbrains.com>
2026-03-31 00:00:54 +0200

    lab9 compilation fixes

--- src/lab9/models.kt
+++ src/lab9/models.kt
@@ -1,9 +1,9 @@
 package lab9
 
 // TODO Implement Department entity from Task 1
-class Department
+class Department(val departmentName: String) // provisional implementation, replace with real one
 
 // TODO Implement EmployeeID entity from Task 1
 
 // TODO Implement Employee entity from Task 1
-class Employee
+class Employee(val name: String, val department: Department) // provisional implementation, replace with real one
