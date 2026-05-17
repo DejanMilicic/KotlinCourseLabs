c097f945daf62dab906c48e9d164387508b49c51
macbook <macbook@192.168.1.10>
2026-05-17 15:06:53 +0200

    Lab2-Task2 - Pair of highest sum functional

--- src/lab2/task2/FindPairOfHighestSumFunctional.kt
+++ src/lab2/task2/FindPairOfHighestSumFunctional.kt
@@ -19,7 +19,8 @@ import lab2.common.isEqualsTo
  */
 
 internal fun List<Int>.findHighestSumPairFunctional(): Pair<Int, Int> {
-    TODO("Implement me!!")
+    return this.flatMapIndexed { index, num -> this.drop(index + 1).map {otherNum -> num to otherNum} }
+        .maxByOrNull { it.first + it.second }!!
 }
 
 fun main() {
