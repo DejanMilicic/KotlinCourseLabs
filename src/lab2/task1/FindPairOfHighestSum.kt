f3f566b18328b83ee5467e37fa7431be69e42008
macbook <macbook@192.168.1.10>
2026-05-17 15:01:50 +0200

    Lab2-Task1 - Find pair of highest sum

--- src/lab2/task1/FindPairOfHighestSum.kt
+++ src/lab2/task1/FindPairOfHighestSum.kt
@@ -17,7 +17,22 @@ import lab2.common.isEqualsTo
  */
 
 internal fun List<Int>.findHighestSumPair(): Pair<Int, Int> {
-    TODO("Implement me!!")
+    var highestSum = Int.MIN_VALUE
+    var highestPair: Pair<Int, Int>? = null
+
+    for ((index, num) in this.withIndex()) {
+        for (j in index + 1 until this.size) {
+            val otherNum = this[j]
+            val newSum = num + otherNum
+
+            if (newSum >= highestSum) {
+                highestSum = newSum
+                highestPair = Pair(num, otherNum)
+            }
+        }
+    }
+
+    return highestPair!!
 }
 
 fun main() {
