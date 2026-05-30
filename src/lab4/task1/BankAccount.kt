45cfd328d8a742d00ad875a7dd37e2261e0229ca
Veljko Mihajlović <81825352+Veksa123123@users.noreply.github.com>
2026-05-30 17:33:51 +0200

    Lab4

--- src/lab4/task1/BankAccount.kt
+++ src/lab4/task1/BankAccount.kt
@@ -32,21 +32,38 @@ package lab4.task1
  *      The initial balance for this constructor should always be set to 0.
  */
 
+class BankAccount(val accountNumber: String, val accountHolderName: String, private var balance: Double) {
+
+    constructor(accountNumber: String, accountHolderName: String) : this(accountNumber, accountHolderName, 0.0)
+
+    fun deposit(amount: Double) {
+        if (amount > 0) {
+            balance += amount
+        }
+    }
+
+    fun withdraw(amount: Double): Boolean {
+        return if (amount <= balance) {
+            balance -= amount
+            true
+        } else false
+    }
+
+    fun getBalance(): Double {
+        return balance
+    }
+
+    fun displayAccountInfo() {
+        println("Account Holder: $accountHolderName")
+        println("Account Number: $accountNumber")
+        println("Balance: $balance")
+    }
+}
 
 fun main() {
-    TODO("Uncomment the lines below after the Bank Account class is implemented.")
-    // Create a Bank Account
-    // val account = BankAccount("123456789", "John Doe")
-
-    // Display account information
-    // account.displayAccountInfo()
-
-    // Deposit some money
-    // account.deposit(1000.0)
-
-    // Withdraw some money
-    // account.withdraw(500.0)
-
-    // Display updated account information
-    // account.displayAccountInfo()
+    val account = BankAccount("123456789", "John Doe")
+    account.displayAccountInfo()
+    account.deposit(1000.0)
+    account.withdraw(500.0)
+    account.displayAccountInfo()
 }
