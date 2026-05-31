340b90d08b07cbbf051232767d6bfb114607f33e
dj-iki <milutindjikandic@gmail.com>
2026-05-31 18:08:03 +0200

    feat: solution to the 1st task in lab6/taskProjections

--- src/lab6/taskProjections/task1/Task.kt
+++ src/lab6/taskProjections/task1/Task.kt
@@ -7,16 +7,14 @@ package lab6.taskProjections.task1
  * Then, you need to modify code somehow to be able to create topRatedPostman and juniorPostman:
  *  - The topRatedPostman can send ONLY express postcards
  *  - The juniorPostman can send both regular and express postcards
+ **/
 
-
-**/
-
-interface Sender {
-    fun send(item: Any)
+interface Sender<in T> {
+    fun send(item: T)
 }
 
-class MailBox(private var box: Any? = null): Sender {
-    override fun send(item: Any) {
+class MailBox<T>(private var box: T? = null) : Sender<T> {
+    override fun send(item: T) {
         printCurrentBoxState()
         println("Sending the box: $item!")
         box = item
@@ -25,19 +23,20 @@ class MailBox(private var box: Any? = null): Sender {
     private fun printCurrentBoxState() {
         if (box != null) {
             println("I have a box: $box!")
-        } else  {
+        } else {
             println("I have nothing")
         }
     }
 
 }
 
-class Postman(private val mailboxes: List<Sender>): Sender {
-    override fun send(item: Any) {
+class Postman<in T>(private val mailboxes: List<Sender<T>>) : Sender<T> {
+    override fun send(item: T) {
         mailboxes.forEach { it.send(item) }
     }
 
 }
+
 interface Delivery
 
 open class Postcard(open val origin: String) : Delivery
@@ -45,14 +44,18 @@ open class Postcard(open val origin: String) : Delivery
 data class ExpressPostcard(val priceEuro: Int, override val origin: String) : Postcard(origin)
 
 fun main() {
-    // TODO: This code should became compilable
-//    val postcardStorage = MailBox<Postcard>()
-//    val expressPostcardStorage = MailBox<ExpressPostcard>()
+
+    val postcardStorage = MailBox<Postcard>()
+    val expressPostcardStorage = MailBox<ExpressPostcard>()
 
     val expressPostcard = ExpressPostcard(15, "Serbia")
     val postcard = Postcard("Germany")
 
-    // TODO: add code to create topRatedPostman and juniorPostman.
-    //  The topRatedPostman can send ONLY express postcards
-    //  The juniorPostman can send both regular and express postcards
+    val topRatedPostman = Postman<ExpressPostcard>(listOf(expressPostcardStorage))
+    topRatedPostman.send(expressPostcard)
+
+
+    val juniorPostman = Postman<Postcard>(listOf(postcardStorage))
+    juniorPostman.send(postcard)
+    juniorPostman.send(expressPostcard)
 }
