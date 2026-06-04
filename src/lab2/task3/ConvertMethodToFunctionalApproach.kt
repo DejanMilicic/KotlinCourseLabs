9a93bd0563ccb41b054f96641d96457a7aaaeafb
Aleksanda-Saska <radovanovic.aleksandra.002@gmail.com>
2026-06-04 16:06:13 +0200

    Lab2Task3-completed

--- src/lab2/task3/ConvertMethodToFunctionalApproach.kt
+++ src/lab2/task3/ConvertMethodToFunctionalApproach.kt
@@ -8,7 +8,7 @@ import kotlin.math.abs
  *
  * The function is written with the imperative approach, and the task of this assignment is to refactor it to the
  * functional approach.
- *
+ *ir
  * Constraints:
  * List has at least two integers.
  *
@@ -16,26 +16,13 @@ import kotlin.math.abs
  * @return The pair with the biggest difference between its elements.
  */
 
-internal fun List<Int>.findPairWithBiggestDifference(): Pair<Int, Int> {
-    // TODO refactor me to functional approach and make tests pass!!!
-    var resultPair: Pair<Int, Int>? = null
-    var biggestDifference = Int.MIN_VALUE
+internal fun List<Int>.findPairWithBiggestDifference(): Pair<Int, Int> = indices
+    .flatMap { i ->
+    (i + 1 until size)
+        .map{ j -> Pair(this[i], this[j]) }}
+    .maxBy{ abs(it.first - it.second) }
 
-    for (i in this.indices) {
-        for (j in (i + 1) until this.size) {
-            val first = this[i]
-            val second = this[j]
-            val absDifference = abs(first - second)
 
-            if (absDifference >= biggestDifference) {
-                biggestDifference = absDifference
-                resultPair = Pair(first, second)
-            }
-        }
-    }
-
-    return resultPair!!
-}
 
 fun main() {
     val nums = listOf(743, 284, 677, -753, 995, -934, 102, 903, -83, -760, 77, -420)
