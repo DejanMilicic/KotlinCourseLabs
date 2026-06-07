7743d6ba344fb70ebd5852d1c590db5c1a3394c3
macbook <macbook@192.168.1.10>
2026-06-07 13:59:26 +0200

    Lab8 - Update Movie

--- src/lab8/MovieDB.kt
+++ src/lab8/MovieDB.kt
@@ -4,15 +4,15 @@ class MovieDB (
     private val movies: List<Movie>
 ): MovieDBApi {
     override fun getAllMoviesByActor(actor: MovieActor): List<Movie> =
-        movies
-            .filter { movie ->
-            actor in movie.actors
-        }
+        movies.filter { actor in it.actors }
 
-    override fun getMoviesWithBiggestProfit(numOfMovies: Int): List<Movie> =
-        movies
+    override fun getMoviesWithBiggestProfit(numOfMovies: Int): List<Movie> {
+        require(numOfMovies >= 0)
+
+        return movies
             .sortedByDescending { it.revenue - it.budget }
             .take(numOfMovies)
+    }
 
     override fun getBestRatedMovieByActor(actor: MovieActor): Movie? =
         movies
@@ -20,17 +20,18 @@ class MovieDB (
             .maxByOrNull { it.rating }
 
     override fun getAllMoviesByYear(year: Int): List<Movie> =
-        movies
-            .filter { it.releaseDate.year == year }
+        movies.filter { it.releaseDate.year == year }
 
     override fun getAllMoviesByGenre(genre: String): List<Movie> =
-        movies
-            .filter {genre in it.genres}
+        movies.filter {genre in it.genres}
 
-    override fun getBestRatedMovies(numOfMovies: Int): List<Movie> =
-        movies
+    override fun getBestRatedMovies(numOfMovies: Int): List<Movie> {
+        require(numOfMovies >= 0)
+
+        return movies
             .sortedByDescending { it.rating }
             .take(numOfMovies)
+    }
 
     override fun getDirectorWithMostMoviesDirected(): MovieDirector =
         movies
@@ -40,32 +41,28 @@ class MovieDB (
             ?.key ?: throw NoSuchElementException("There is no movie")
 
     override fun getActorsWithMostCostarredMovies(): List<Pair<MovieActor, MovieActor>> {
-
         val pairCounts = movies
             .flatMap { movie ->
-                val actors = movie.actors
-
-                val pairs = mutableListOf<Pair<MovieActor, MovieActor>>()
-
-                for (i in actors.indices) {
-                    for (j in i + 1 until actors.size) {
-                        val sorted = listOf(actors[i], actors[j])
-                            .sortedBy { it.name }
-
-                        pairs.add(sorted[0] to sorted[1])
-                    }
+                movie.actors.flatMapIndexed { index, actor ->
+                    movie.actors
+                        .drop(index + 1)
+                        .map { otherActor ->
+                            listOf(actor, otherActor)
+                                .sortedBy { it.name }
+                                .let { it[0] to it[1] }
+                        }
                 }
-
-                pairs
             }
             .groupingBy { it }
             .eachCount()
 
-        val max = pairCounts.maxByOrNull { it.value }?.value ?: 0
+        val maxCount = pairCounts.maxOfOrNull { it.value } ?: 0
 
         return pairCounts
-            .filter { it.value == max }
-            .map { it.key }
-            .sortedWith(compareBy( { it.first.name }, { it.second.name }))
+            .filter { it.value == maxCount }
+            .keys
+            .sortedWith(compareBy<Pair<MovieActor, MovieActor>> { it.first.name }
+                .thenBy { it.second.name }
+            )
     }
 }
