06c95e97188d5736a69b6bec04e93bc958e194c0
dusanbakurski <bakurskidusan5@gmail.com>
2026-06-08 01:15:00 +0200

    [LAB6] Task Projections 5

--- src/lab6/taskProjections/task5/Task.kt
+++ src/lab6/taskProjections/task5/Task.kt
@@ -9,25 +9,38 @@ package lab6.taskProjections.task5
  *
  * [2]
  * Create a new abstract class "Builder" that generates instances of T based on parameter of P type.
- * This class cannot produce P and consume T:
+ * This class cannot produce P and consume T: <- Isnt this a same
  *     - add an abstract "build" function that accepts P and returns T;
  * Create a new class "IntegerBuilder", that implements Builder and can convert String to Int
 **/
+class Printer<in T> {
+    fun print(item: T) {
+        println(item)
+    }
+}
+
+abstract class Builder<in P, out T> {
+    abstract fun build(param: P): T
+}
+
+class IntegerBuilder : Builder<String, Int>() {
+    override fun build(param: String): Int {
+        return param.toInt()
+    }
+}
 
 fun main() {
     // [1]
-    // TODO: uncomment me
-//    val integerPrinter = Printer<Int>()
-//    val stringPrinter = Printer<String>()
-//
-//    integerPrinter.print(2)
-//    stringPrinter.print("Bla bla bla")
+    val integerPrinter = Printer<Int>()
+    val stringPrinter = Printer<String>()
+
+    integerPrinter.print(2)
+    stringPrinter.print("Bla bla bla")
 
     println("________")
 
     // [2]
-    // TODO: uncomment me
-//    val intBuilder = IntegerBuilder()
-//    val x = intBuilder.build("1")
-//    println("We build [ $x ]")
+    val intBuilder = IntegerBuilder()
+    val x = intBuilder.build("1")
+    println("We build [ $x ]")
 }
