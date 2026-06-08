0acc125f100fb373c130be55ad3771ec5cb2e517
dusanbakurski <bakurskidusan5@gmail.com>
2026-06-08 21:04:00 +0200

    [LAB9]

--- src/lab9/EmployeesRunner.kt
+++ src/lab9/EmployeesRunner.kt
@@ -3,11 +3,27 @@ package lab9
 import common.FileReader
 
 fun parseEmployees(employeesCSVLines: List<String>): List<Employee> {
-    TODO("Implement parsing of employees")
+    val dataLines = employeesCSVLines.subList(1, employeesCSVLines.size)
+    val employeeList = mutableListOf<Employee>()
+
+    dataLines.forEach {
+        val data = it.split(",")
+        employeeList.add(
+            Employee(
+                EmployeeID(data[0]),
+                data[1],
+                Department(data[2]),
+                data[3].toInt(),
+                data[4].split("|")
+            )
+        )
+    }
+
+    return employeeList
 }
 
 fun newEmployeeApi(employees: List<Employee>): EmployeeApi {
-    TODO("Instantiate EmployeeApi")
+    return EmployeesPortal(employees)
 }
 
 fun main() {
--- src/lab9/models.kt
+++ src/lab9/models.kt
@@ -1,9 +1,39 @@
 package lab9
 
-// TODO Implement Department entity from Task 1
-class Department(val departmentName: String) // provisional implementation, replace with real one
+data class Department(val departmentName: String)
 
-// TODO Implement EmployeeID entity from Task 1
+@JvmInline
+value class EmployeeID(val id: String)
 
-// TODO Implement Employee entity from Task 1
-class Employee(val name: String, val department: Department) // provisional implementation, replace with real one
+class Employee(val employeeID: EmployeeID,
+               val name: String,
+               val department: Department,
+               val salary: Int,
+               val skills: List<String>)
+
+class EmployeesPortal(private val employees: List<Employee>) : EmployeeApi{
+
+    override fun findHighestPaidEmployee(): Employee? = employees.maxByOrNull { it.salary }
+
+    override fun getEmployeesByDepartment(department: Department): List<Employee> = employees.filter { it.department == department }
+
+    override fun getEmployeesBySalaryRange(salaryRange: IntRange): List<Employee> = employees.filter { it.salary in salaryRange }
+
+    override fun calculateAverageSalaryByDepartment(department: Department): Double {
+        val employeesOfDepartment = getEmployeesByDepartment(department)
+        return employeesOfDepartment.sumOf { it.salary }.toDouble() / employeesOfDepartment.count()
+    }
+
+    override fun findMostCommonSkill(): String {
+        val skillCounts = mutableMapOf<String, Int>()
+
+        employees.forEach { employee ->
+            employee.skills.forEach { skill ->
+                skillCounts[skill] = skillCounts.getOrDefault(skill, 0) + 1
+            }
+        }
+
+        return skillCounts.maxByOrNull { it.value }?.key ?: ""
+    }
+
+}
