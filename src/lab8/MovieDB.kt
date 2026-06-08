6db06db160e00e889bf32e00990e09aaa288cc8a
Markeyen <ljubojamateja@gmail.com>
2026-06-08 23:51:30 +0200

    lab8 finished

--- src/lab8/MovieDB.kt
+++ src/lab8/MovieDB.kt
@@ -1,5 +1,7 @@
 package lab8
 
+import java.time.LocalDate
+
 /**
  * Task: Write a MovieDB class that implements the MovieDBApi interface.
  *
@@ -8,3 +10,54 @@ package lab8
  * Implement methods defined by MovieDBApi.
  *
  */
+
+class MovieDB(val movieDB: List<Movie>) : MovieDBApi {
+    override fun getAllMoviesByActor(actor: MovieActor): List<Movie> {
+        return movieDB.filter { it.actors.contains(actor) }
+    }
+
+    override fun getMoviesWithBiggestProfit(numOfMovies: Int): List<Movie> {
+        return movieDB.sortedByDescending { it.revenue - it.budget }.take(numOfMovies)
+    }
+
+    override fun getBestRatedMovieByActor(actor: MovieActor): Movie? {
+        return movieDB.filter { it.actors.contains(actor) }.maxByOrNull { it.rating }
+    }
+
+    override fun getAllMoviesByYear(year: Int): List<Movie> {
+        return movieDB.filter { it.releaseDate.year == year }
+    }
+
+    override fun getAllMoviesByGenre(genre: String): List<Movie> {
+        return movieDB.filter { it.genres.contains(genre) }
+    }
+
+    override fun getBestRatedMovies(numOfMovies: Int): List<Movie> {
+        return movieDB.sortedByDescending { it.rating }.take(numOfMovies)
+    }
+
+    override fun getDirectorWithMostMoviesDirected(): MovieDirector {
+        return movieDB.groupBy { it.director }.maxByOrNull { it.value.size }!!.key
+    }
+
+    override fun getActorsWithMostCostarredMovies(): List<Pair<MovieActor, MovieActor>> {
+        val pairCount = mutableMapOf<Pair<MovieActor, MovieActor>, Int>()
+
+        for (movie in movieDB) {
+            val actor = movie.actors
+            for (i in actor.indices) {
+                for (j in i + 1 until actor.size) {
+                    val actor1 = if (actor[i].name < actor[j].name) actor[i] else actor[j]
+                    val actor2 = if (actor[i].name < actor[j].name) actor[j] else actor[i]
+                    val pair = Pair(actor1, actor2)
+                    pairCount[pair] = (pairCount[pair] ?: 0) + 1
+                }
+            }
+        }
+        val max = pairCount.values.maxOrNull() ?: return emptyList()
+        return pairCount.filter { it.value == max }.keys.toList()
+            .sortedWith(compareBy({ it.first.name }, { it.second.name }))
+    }
+
+
+}
--- src/lab8/MovieDBRunner.kt
+++ src/lab8/MovieDBRunner.kt
@@ -1,15 +1,40 @@
 package lab8
 
 import common.FileReader
+import java.time.LocalDate
 
 
-private fun parseMovies(moviesLines: List<String>): List<Movie> {
-    TODO("Implement parsing of the file content")
+fun parseMovies(moviesLines: List<String>): List<Movie> {
+    return moviesLines.drop(1).map { line ->
+        val column = line.split(";")
+        val buget = column[0].toLong()
+        val ganres = column[1].split(",").map { it.trim() }
+        val title = column[2]
+        val rating = column[3].toDouble()
+        val releaseDate = LocalDate.parse(column[4])
+        val revenue = column[5].toLong()
+        val runtime = column[6].toDouble().toInt()
+        val cast = column[7].split(",").filter { it.isNotBlank() }.map { MovieActor(it.trim()) }
+        val director = MovieDirector(column[8].trim())
+
+        Movie(
+            title = title,
+            budget = buget,
+            revenue = revenue,
+            releaseDate = releaseDate,
+            runtimeInMinutes = runtime,
+            director = director,
+            genres = ganres,
+            actors = cast,
+            rating = rating
+
+        )
+    }
 }
 
 fun main() {
     val moviesCSVFile = FileReader.readFileInResources("lab8/movies.csv")
     val movies = parseMovies(moviesCSVFile)
 
-    val movieDBApi : MovieDBApi = TODO("Instantiate MovieDB")
+    val movieDBApi: MovieDBApi = MovieDB(movies)
 }
