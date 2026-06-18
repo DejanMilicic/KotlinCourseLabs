33d59797d831e076d8c2f00e1c14391b05bf543d
Dusan Radisavljevic <dusanradisavljevic172@gmail.com>
2026-06-19 00:09:14 +0200

    Lab1 Task3

--- src/lab1/task3/ScrabbleWordScoreCalculator.kt
+++ src/lab1/task3/ScrabbleWordScoreCalculator.kt
@@ -31,8 +31,25 @@ package lab1.task3
  *
  */
 
+internal fun getLetterScore(letter : Char): Int {
+    return when (letter) {
+        'a', 'e', 'i', 'o', 'u', 'l', 'n', 'r', 's', 't' -> 1
+        'd', 'g' -> 2
+        'b', 'c', 'm', 'p' -> 3
+        'f', 'h', 'v', 'w', 'y' -> 4
+        'k' -> 5
+        'j', 'x' -> 8
+        'q', 'z' -> 10
+        else -> 0
+    }
+}
+
 internal fun calculateWordScrabbleScore(word: String): Int {
-    TODO("Implement me!!!")
+    var score = 0
+    for(letter in word) {
+        score += getLetterScore(letter)
+    }
+    return score
 }
 
 fun main() {
