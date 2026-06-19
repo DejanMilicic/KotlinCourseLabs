322ce7185b76713af3e094fe702c69243887f8b3
Dusan Radisavljevic <dusanradisavljevic172@gmail.com>
2026-06-20 01:49:15 +0200

    Fixed changes

--- src/lab1/task1/GradeCalculator.kt
+++ src/lab1/task1/GradeCalculator.kt
@@ -40,17 +40,17 @@ import java.util.Scanner
  */
 
 internal fun calculateGrade(score: Int): Int {
-    if(score !in 0..100) {
+    if (score !in 0..100) {
         throw IllegalArgumentException("Invalid score. Please enter a score between 0 and 100")
     }
 
-    return when(score) {
+    return when (score) {
         in 91..100 -> 10
         in 81..90 -> 9
         in 71..80 -> 8
         in 61..70 -> 7
         in 51..60 -> 6
-        else -> throw IllegalArgumentException("Invalid score. Score is too low for grading")
+        else -> 5
     }
 }
 
