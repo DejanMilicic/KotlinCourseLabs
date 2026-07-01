94a53c3858d3230c03b0acd934e7de48f0612f76
Uros <druros032@gmail.com>
2026-07-01 15:15:09 +0200

    Here is lab9

--- src/lab9/models.kt
+++ src/lab9/models.kt
@@ -1,9 +1,50 @@
 package lab9
 
-// TODO Implement Department entity from Task 1
-class Department(val departmentName: String) // provisional implementation, replace with real one
 
-// TODO Implement EmployeeID entity from Task 1
+data class Department(val departmentName: String) // provisional implementation, replace with real one
 
-// TODO Implement Employee entity from Task 1
-class Employee(val name: String, val department: Department) // provisional implementation, replace with real one
+
+@JvmInline
+value class EmployeeID(val id: String)
+
+data class Employee(
+		val employeeID: EmployeeID,
+		val name: String,
+		val department: Department,
+		val salary: Int,
+		val skills: List<String>) // provisional implementation, replace with real one
+
+
+class EmployeesPortal(private val employees: List<Employee>) : EmployeeApi {
+	override fun findHighestPaidEmployee(): Employee? {
+		return employees
+				.maxByOrNull { it.salary }
+	}
+
+	override fun getEmployeesByDepartment(department: Department): List<Employee> {
+		return employees
+				.filter { it.department == department }
+	}
+
+	override fun getEmployeesBySalaryRange(salaryRange: IntRange): List<Employee> {
+		return employees
+				.filter { it.salary in salaryRange }
+	}
+
+	override fun calculateAverageSalaryByDepartment(department: Department): Double {
+		return employees
+				.filter { it.department == department }
+				.map { it.salary }
+				.average()
+
+	}
+
+	override fun findMostCommonSkill(): String {
+		return employees
+				.flatMap { it.skills }
+				.groupBy { it }
+				.maxBy { it.value.size }
+				.key
+	}
+
+}
