9b5a72224a4e6c0b3157c216fdbc5aa0cdbe5ba0
Uros <druros032@gmail.com>
2026-07-01 14:56:14 +0200

    Here is task1 from lab6

--- src/lab6/task1/SimpleGenerics.kt
+++ src/lab6/task1/SimpleGenerics.kt
@@ -14,9 +14,9 @@ package lab6.task1
 /**
  * A binary tree node that holds some value.
  */
-private data class TreeNode(val value: Any) {
-    var left: TreeNode? = null
-    var right: TreeNode? = null
+private data class TreeNode<T>(val value:T) {
+    var left: TreeNode<T>? = null
+    var right: TreeNode<T>? = null
 }
 
 /**
@@ -35,6 +35,7 @@ private data class Decision(val question: String, val answerLeft: String, val an
  * Decision value. This code is not yet compilable, but it will become compilable as soon as you make a generic
  * TreeNode class.
  */
+
 private fun printNodes(): String {
     // PLEASE DON'T CHANGE THE CODE OF THIS FUNCTION.
     val richardI = Person("Richard I", 1157, "The Lionheart")
@@ -42,8 +43,7 @@ private fun printNodes(): String {
 
     val decisionI = Decision("Do you choose a red or a blue pill?", "Red", "Blue")
     val decisionTree = TreeNode(decisionI)
-    TODO("Uncomment code bellow and make the file compilable")
-//    return "${genealogyTree.value.name} ${decisionTree.value.answerLeft}"
+    return "${genealogyTree.value.name} ${decisionTree.value.answerLeft}"
 }
 
 fun main() {
