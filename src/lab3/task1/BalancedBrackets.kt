47b4fb3a736379d63a9a97ff67b4ec75bc85f257
Uros <druros032@gmail.com>
2026-07-01 15:29:56 +0200

    Here is task1 from lab3

--- src/lab3/task1/BalancedBrackets.kt
+++ src/lab3/task1/BalancedBrackets.kt
@@ -26,7 +26,18 @@ package lab3.task1
 
 
 internal fun isExpressionBalanced(expression: String): Boolean {
-    TODO("Implement me!!!")
+    val stack = ArrayDeque<Char>()
+
+    for (char in expression) {
+        when (char) {
+            '(', '[', '{' -> stack.addLast(char)
+            ')' -> if (stack.isEmpty() || stack.removeLast() != '(') return false
+            ']' -> if (stack.isEmpty() || stack.removeLast() != '[') return false
+            '}' -> if (stack.isEmpty() || stack.removeLast() != '{') return false
+        }
+    }
+
+    return stack.isEmpty()
 }
 
 fun main() {
