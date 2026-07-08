16fff794d02b673e23ab6ab0d450faa7d70391d7
urosrelic <urosrelicc@gmail.com>
2026-07-08 12:47:01 +0200

    finish lab1 task1

--- src/lab1/task1/GradeCalculator.kt
+++ src/lab1/task1/GradeCalculator.kt
@@ -40,7 +40,16 @@ import java.util.Scanner
  */
 
 internal fun calculateGrade(score: Int): Int {
-    TODO("Implement me!!!")
+    require(score in 0..100) { "Invalid score. Please enter a score between 0 and 100." }
+
+    return when (score) {
+        in 91..100 -> 10
+        in 81..90 -> 9
+        in 71..80 -> 8
+        in 61..70 -> 7
+        in 51..60 -> 6
+        else -> 5
+    }
 }
 
 fun main() {
