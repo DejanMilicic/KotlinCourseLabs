15cf59e29e0f17618f8032446602d26fee0b840f
kjov245 <kristijan.grej2004@gmail.com>
2026-07-09 17:55:55 +0200

    Implement Lab7 League Preview

--- src/lab7/LeagePreview.kt
+++ src/lab7/LeagePreview.kt
@@ -3,16 +3,40 @@ package lab7
 import common.FileReader
 
 internal fun parseFixtures(fixturesText: List<String>): List<Fixture> {
-    TODO("Parse fixtures text to list of Fixtures.")
+    val teams = HashMap<String, Team>()
+
+    return fixturesText
+
+        .drop(1)
+        .map { fixtureText ->
+            val fixtureData = fixtureText.split(",")
+
+            val fixtureId = fixtureData[0].toInt()
+            val homeTeam = teams.getOrPut(fixtureData[2]) { Team(fixtureData[2]) }
+            val awayTeam = teams.getOrPut(fixtureData[4]) { Team(fixtureData[4]) }
+            val (homeTeamScore, awayTeamScore) = fixtureData[3].trim()
+                .split("-")
+                .let { scores -> scores.first().toInt() to scores[1].toInt() }
+
+            fixtureId to Match(homeTeam, awayTeam, homeTeamScore, awayTeamScore)
+        }.fold(mutableMapOf<Int, List<Match>>()) { fixtureIdsToMatches, (fixtureId: Int, match: Match) ->
+            val matches = fixtureIdsToMatches.getOrDefault(fixtureId, emptyList())
+
+            fixtureIdsToMatches[fixtureId] = matches + match
+
+            fixtureIdsToMatches
+        }.entries.map { (fixtureId, matches) -> Fixture(fixtureId, matches) }
 }
 
 fun main() {
     val fixturesText = FileReader.readFileInResources("/lab7/fixtures.csv")
     val fixtures: List<Fixture> = parseFixtures(fixturesText)
-    val teams = TODO("Parse all teams from fixtures")
+    val teams = fixtures.flatMap { it.matches }
+        .flatMap { listOf(it.homeTeam, it.awayTeam) }
+        .distinct()
 
     // Create league object
-    val league: LeagueApi = TODO("Instantiate league: League(teams, fixtures)")
+    val league: LeagueApi = League(teams, fixtures)
     league.displayLeagueTable()
 
     league.displayLeagueTableAtFixture(13)
