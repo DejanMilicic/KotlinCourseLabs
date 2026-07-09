2f59e645acdad04112421f2143e40dd4185c6adf
kjov245 <kristijan.grej2004@gmail.com>
2026-07-09 16:17:33 +0200

    Implement Lab3 Task1 Balanced Brackets

--- src/lab3/task1/BalancedBrackets.kt
+++ src/lab3/task1/BalancedBrackets.kt
@@ -1,5 +1,7 @@
 package lab3.task1
 
+import java.util.Stack
+
 /**
  * Task1: Balanced Brackets (Parentheses) Problem
  *
@@ -26,7 +28,19 @@ package lab3.task1
 
 
 internal fun isExpressionBalanced(expression: String): Boolean {
-    TODO("Implement me!!!")
+    val stack = ArrayDeque<Char>()
+
+    for(char in expression) {
+        when(char) {
+            '(','{','[' -> stack.addLast(char)
+
+            ')' -> if(stack.isEmpty() || stack.removeLast() != '(') return false
+            '}' -> if(stack.isEmpty() || stack.removeLast() != '{') return false
+            ']' -> if(stack.isEmpty() || stack.removeLast() != '[') return false
+        }
+    }
+
+    return stack.isEmpty()
 }
 
 fun main() {
