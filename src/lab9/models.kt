85fee01ab7f5cc96545dafd2d3d031f33472e638
kjov245 <kristijan.grej2004@gmail.com>
2026-07-09 18:16:34 +0200

    Implement Lab9 models

--- src/lab9/models.kt
+++ src/lab9/models.kt
@@ -1,9 +1,21 @@
 package lab9
 
 // TODO Implement Department entity from Task 1
-class Department(val departmentName: String) // provisional implementation, replace with real one
+data class Department(
+    val departmentName: String
+) // provisional implementation, replace with real one
 
 // TODO Implement EmployeeID entity from Task 1
+@JvmInline
+value class EmployeeID (
+    val id: String
+)
 
 // TODO Implement Employee entity from Task 1
-class Employee(val name: String, val department: Department) // provisional implementation, replace with real one
+data class Employee(
+    val employeeID: EmployeeID,
+    val name: String,
+    val department: Department,
+    val salary: Int,
+    val skills: List<String>) // provisional implementation, replace with real one
+
