7d5fe6ee84f98946290c6afd3935811fa00e1899
kjov245 <kristijan.grej2004@gmail.com>
2026-07-09 18:07:22 +0200

    Implement Lab8 MovieDB Runner

--- src/lab8/MovieDBRunner.kt
+++ src/lab8/MovieDBRunner.kt
@@ -1,15 +1,34 @@
 package lab8
 
 import common.FileReader
+import java.time.LocalDate
 
 
 private fun parseMovies(moviesLines: List<String>): List<Movie> {
-    TODO("Implement parsing of the file content")
+    return moviesLines
+        .drop(1)
+        .map { line ->
+            val parts = line.split(";")
+            val budget = parts[0].trim().toLong()
+            val genres = parts[1].trim().split(",").map { it.trim() }
+            val title = parts[2].trim()
+            val rating = parts[3].trim().toDouble()
+
+            val relaseDate = LocalDate.parse(parts[4])
+            val revenue = parts[5].trim().toLong()
+            val runtime = parts[6].trim().toDouble().toInt()
+            val actors = parts[7].trim().split(",")
+                .filter { it.isNotBlank() }
+                .map { MovieActor(it.trim()) }
+            val director = MovieDirector(parts[8].trim())
+
+            Movie(title, budget, revenue, relaseDate, runtime, director, genres, actors, rating)
+        }
 }
 
 fun main() {
     val moviesCSVFile = FileReader.readFileInResources("lab8/movies.csv")
     val movies = parseMovies(moviesCSVFile)
 
-    val movieDBApi : MovieDBApi = TODO("Instantiate MovieDB")
+    val movieDBApi : MovieDBApi = MovieDB(movies)
 }
