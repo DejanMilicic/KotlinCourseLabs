885e3e6ffcaa059cf1e0e8efe91655c6216f64a8
Marjan Martinov <dmi.109.23@student.pmf.uns.ac.rs>
2026-07-10 00:24:49 +0200

    Finished the task.

--- src/lab9/EmployeesRunner.kt
+++ src/lab9/EmployeesRunner.kt
@@ -3,16 +3,25 @@ package lab9
 import common.FileReader
 
 fun parseEmployees(employeesCSVLines: List<String>): List<Employee> {
-    TODO("Implement parsing of employees")
+    return employeesCSVLines.drop(1).map { line ->
+        val parts = line.split(",")
+        val employeeid = EmployeeID(parts[0])
+        val name = parts[1]
+        val dep = Department(parts[2])
+        val salary = parts[3].toInt()
+        val skills = parts[4].split("|")
+
+        Employee(employeeid, name, dep, salary, skills)
+    }
 }
 
 fun newEmployeeApi(employees: List<Employee>): EmployeeApi {
-    TODO("Instantiate EmployeeApi")
+    return EmployeePortal(employees)
 }
 
 fun main() {
-    val employeesCSVLines = FileReader.readFileInResources("exercise9/employees.csv")
+    val employeesCSVLines = FileReader.readFileInResources("lab9/employees.csv")
     val employees = parseEmployees(employeesCSVLines)
 
-    val employeeApi : EmployeeApi = newEmployeeApi(employees)
+    val employeeApi: EmployeeApi = newEmployeeApi(employees)
 }
