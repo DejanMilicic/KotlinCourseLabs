adc83a7fd145bc327987d409dc42f7dc52ca91b3
NikolinaDobanovacki <nikolinadobanovacki22@gmail.com>
2026-07-09 23:57:17 +0200

    lab2 task3 done

--- src/lab2/task3/ConvertMethodToFunctionalApproach.kt
+++ src/lab2/task3/ConvertMethodToFunctionalApproach.kt
@@ -17,24 +17,9 @@ import kotlin.math.abs
  */
 
 internal fun List<Int>.findPairWithBiggestDifference(): Pair<Int, Int> {
-    // TODO refactor me to functional approach and make tests pass!!!
-    var resultPair: Pair<Int, Int>? = null
-    var biggestDifference = Int.MIN_VALUE
-
-    for (i in this.indices) {
-        for (j in (i + 1) until this.size) {
-            val first = this[i]
-            val second = this[j]
-            val absDifference = abs(first - second)
-
-            if (absDifference >= biggestDifference) {
-                biggestDifference = absDifference
-                resultPair = Pair(first, second)
-            }
-        }
-    }
-
-    return resultPair!!
+    return this.flatMapIndexed { i, first ->
+        this.drop(i + 1).map { second -> Pair(first, second) }
+    }.maxBy { abs(it.first - it.second) }
 }
 
 fun main() {
