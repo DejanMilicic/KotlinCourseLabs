166b5432353fd915250403278d59d53849d7f39e
Marjan Martinov <dmi.109.23@student.pmf.uns.ac.rs>
2026-07-10 20:01:47 +0200

    Updated the var max value, the loop range and the place where sum is initialized.

--- src/lab2/task1/FindPairOfHighestSum.kt
+++ src/lab2/task1/FindPairOfHighestSum.kt
@@ -19,11 +19,10 @@ import lab2.common.isEqualsTo
 internal fun List<Int>.findHighestSumPair(): Pair<Int, Int> {
 
     var max = Int.MIN_VALUE
-    var sum = -1
     var res = Pair(0, 0)
     for (i in this.indices) {
         for (j in i + 1 until this.size) {
-            sum = this[i] + this[j]
+            val sum = this[i] + this[j]
             if (sum >= max) {
                 max = sum
                 res = Pair(this[i], this[j])
